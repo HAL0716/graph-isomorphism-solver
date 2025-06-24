d4969dede997f53812faafe84ce998e9ff666ca3
HAL <207191136+HAL0716@users.noreply.github.com>
2025-06-24 13:53:12 +0900

    make main.cpp (#8)
    * Init Structure
    
    * Add Graph::AdjList
    
    * Add getReversed for Graph::AdjList
    
    * Add Graph::GraphFeat
    
    * Add Graph::Isomorphism
    
    * make main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,25 @@
 #include <iostream>
+#include "AdjList.hpp"
+#include "Isomorphism.hpp"
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    Graph::AdjList adjA;
+    adjA.insert(0,1);
+    adjA.insert(0,2);
+    adjA.insert(1,2);
+    adjA.insert(2,3);
+    adjA.insert(3,1);
+    adjA.insert(3,2);
+
+    Graph::AdjList adjB;
+    adjB.insert(0,3);
+    adjB.insert(0,1);
+    adjB.insert(3,1);
+    adjB.insert(1,2);
+    adjB.insert(2,3);
+    adjB.insert(2,1);
+
+    std::cout << (Graph::Isomorphism::solver(adjA, adjB) ? "Yes" : "No") << std::endl;
+ 
     return 0;
 }
