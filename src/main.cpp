4b190c38d26eb871f5422f4fa41766393acaa53d
HAL0716 <207191136+HAL0716@users.noreply.github.com>
2025-07-02 16:33:36 +0900

    Correction groupIsomorphicGraphs

--- src/main.cpp
+++ src/main.cpp
@@ -5,41 +5,42 @@
 #include "AdjList.hpp"
 #include "Isomorphism.hpp"
 
-namespace fs = std::filesystem;
-
-void showProgress(const std::string& fileA, const std::string& fileB) {
-    std::cout << "Comparing: " << fs::path(fileA).stem().string()
-              << " <-> " << fs::path(fileB).stem().string() << std::endl;
-}
-
 std::vector<std::vector<std::string>> groupIsomorphicGraphs(const std::set<std::string>& filepaths) {
-    std::vector<std::vector<std::string>> groups;
+    std::vector<std::vector<std::string>> groupSet;
+    size_t current = 0;
+    const size_t total = filepaths.size();
 
     for (const auto& filepath : filepaths) {
-        Graph::AdjList adjA;
-        adjA.loadCSV(filepath);
+        ++current;
+        std::cout << "[" << current << "/" << total << "] " << Utils::getBasename(filepath) << std::endl;
 
-        bool added = false;
+        Graph::AdjList targetGraph;
+        targetGraph.loadCSV(filepath);
 
-        for (auto& group : groups) {
-            Graph::AdjList adjB;
-            adjB.loadCSV(group.front());
+        bool foundGroup = false;
 
-            showProgress(filepath, group.front());
+        for (auto& group : groupSet) {
+            const std::string& representativePath = group.front();
+            Graph::AdjList representativeGraph;
+            representativeGraph.loadCSV(representativePath);
 
-            if (Graph::Isomorphism::solver(adjB, adjA)) {
+            std::cout << " <-> " << Utils::getBasename(representativePath) << " : " << std::flush;
+
+            if (Graph::Isomorphism::solver(representativeGraph, targetGraph)) {
+                std::cout << "Yes" << std::endl;
                 group.push_back(filepath);
-                added = true;
+                foundGroup = true;
                 break;
+            } else {
+                std::cout << "No" << std::endl;
             }
         }
 
-        if (!added) {
-            groups.push_back({filepath});
-        }
+        if (!foundGroup)
+            groupSet.emplace_back(std::vector<std::string>{filepath});
     }
-    
-    return groups;
+
+    return groupSet;
 }
 
 int main() {
@@ -51,12 +52,6 @@ int main() {
         auto groups = groupIsomorphicGraphs(files);
 
         std::cout << "Found " << groups.size() << " groups:\n";
-        for (size_t i = 0; i < groups.size(); ++i) {
-            std::cout << "Group " << (i + 1) << ":\n";
-            for (const auto& f : groups[i]) {
-                std::cout << "  " << fs::path(f).filename().string() << "\n";
-            }
-        }
     }
 
     return 0;
